c2964c274f4a28d2294bfd33f9fbb48ba513b9cd
vadimkanevidimka <vadimdd6@gmail.com>
2024-09-09 16:38:02 +0300

    Initialize project repository

---
+++ BackEnd/Web_CV_Hosting/src/CV Hosting Service/Service/Web_CV_Hosting.CV_Hosting_Service/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Web_CV_Hosting.CV_Hosting_Service
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
